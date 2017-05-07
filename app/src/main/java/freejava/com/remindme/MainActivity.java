38e701fe1e7ca1bde4a08d7dc06dcd6821528a3e
rostik <roostik21@gmail.com>
2017-05-07 21:30:07 +0300

    Add menu notification

--- app/src/main/java/freejava/com/remindme/MainActivity.java
+++ app/src/main/java/freejava/com/remindme/MainActivity.java
@@ -2,21 +2,26 @@ package freejava.com.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity{
 
-    Toolbar toolbar;
+    private static final  int LAYOUT = R.layout.activity_main;
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -28,7 +33,11 @@ public class MainActivity extends AppCompatActivity{
                 return false;
             }
         });
-
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 }
