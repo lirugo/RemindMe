78b5ce8403821fa3ef7568965e7a6a208b87d5ef
rostik <roostik21@gmail.com>
2017-05-09 16:07:14 +0300

    Add Listener on Button Notification in menu_drawer

--- app/src/main/java/freejava/com/remindme/MainActivity.java
+++ app/src/main/java/freejava/com/remindme/MainActivity.java
@@ -2,9 +2,12 @@ package freejava.com.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -54,6 +57,28 @@ public class MainActivity extends AppCompatActivity{
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.view_navigation_open,R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId())
+                {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+    }
+
+    private  void showNotificationTab(){
+        viewPager.setCurrentItem(Constatnts.TAB_TWO);
 
     }
 }
